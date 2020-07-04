66ae5d6728fe9380956500d6e2af6d43f09f0d09
Young Kim <ykim297@gmail.com>
2020-07-04 13:54:07 +0900

    Initial Commit

---
+++ Bomapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bomapp
+//
+//  Created by Yong Seok Kim on 2020/07/04.
+//  Copyright © 2020 Yong Seok Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
